265952049d70b34028dcb04f44628a90539befa3
Akshya-G-Dev <akshaygoyal567@gmail.com>
2024-03-22 23:08:52 +0530

    formatting updated

--- database.py
+++ database.py
@@ -7,15 +7,19 @@ from pydantic import BaseModel
 import asyncio
 import dotenv
 import os
+
 dotenv.load_dotenv()
 MONGO_URL = os.getenv("MONGO_URL")
 client = AsyncIOMotorClient(MONGO_URL)
 database = client.coin
 collection = database.get_collection("data")
+
+
 class Token(BaseModel):
     address: str
     name: str
     symbol: str
+
 
 class Txns(BaseModel):
     m5: Dict[str, int]
@@ -23,11 +27,13 @@ class Txns(BaseModel):
     h6: Dict[str, int]
     h24: Dict[str, int]
 
+
 class Volume(BaseModel):
     h24: float
     h6: float
     h1: float
     m5: float
+
 
 class PriceChange(BaseModel):
     m5: float
@@ -35,15 +41,18 @@ class PriceChange(BaseModel):
     h6: float
     h24: float
 
+
 class Liquidity(BaseModel):
     usd: float
     base: float
     quote: float
 
+
 class Info(BaseModel):
     imageUrl: str
     websites: List[Dict[str, str]]
     socials: List[Dict[str, str]]
+
 
 class Pair(BaseModel):
     chainId: str
@@ -61,6 +70,7 @@ class Pair(BaseModel):
     pairCreatedAt: int
     info: Info
 
+
 class Pairs(BaseModel):
     pairs: List[Pair]
 
@@ -69,15 +79,14 @@ class Pairs(BaseModel):
 async def write_to_db():
     with open("data.json") as file:
         data = json.load(file)
-        for token in data['pairs']:
+        for token in data["pairs"]:
             pair = Pair(**token)
             price = pair.priceUsd
             volume = pair.volume
-            await collection.insert_one({
-                "token": token['baseToken']['symbol'],
-                'price': price,
-                'volume': volume.model_dump(),
-                
-            })
-    
-
+            await collection.insert_one(
+                {
+                    "token": token["baseToken"]["symbol"],
+                    "price": price,
+                    "volume": volume.model_dump(),
+                }
+            )
